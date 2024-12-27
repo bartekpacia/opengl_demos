f13dfbc75f682a782f98a691419ecbc5372635d8
Bartek Pacia <barpac02@gmail.com>
2024-12-28 00:19:47 +0100

    add clang-format

--- main.cpp
+++ main.cpp
@@ -1,48 +1,48 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
-void error_callback(int error, const char* description)
-{
-    fprintf(stderr, "Error: %s\n", description);
+void error_callback(int error, const char* description) {
+  fprintf(stderr, "Error: %s\n", description);
 }
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
-int main()
-{
-    glfwSetErrorCallback(error_callback);
+int main() {
+  glfwSetErrorCallback(error_callback);
 
-    if (!glfwInit())
-    {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return 1;
-    }
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW" << std::endl;
+    return 1;
+  }
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", nullptr, nullptr);
-    if (!window)
-    {
-        glfwTerminate();
-        std::cerr << "Failed to open GLFW window" << std::endl;
-        return 1;
-    }
+  int major;
+  int minor;
+  int rev;
+  glfwGetVersion(&major, &minor, &rev);
+  std::cout << "OpenGL version: " << major << "." << minor << "." << rev << std::endl;
 
-    glfwSetKeyCallback(window, key_callback);
-
-    glfwMakeContextCurrent(window);
-
-
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
-    }
-
-
-    glfwDestroyWindow(window);
+  GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", nullptr, nullptr);
+  if (!window) {
     glfwTerminate();
+    std::cerr << "Failed to open GLFW window" << std::endl;
+    return 1;
+  }
+  glfwMakeContextCurrent(window);
 
-    return 0;
+  glfwSetKeyCallback(window, key_callback);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
 }
